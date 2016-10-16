6597dc1fc922a4a7ee5ff973d50709300be8e9af
Seven Du <dujinfang@gmail.com>
2016-10-16 18:14:21 +0800

    show calls

--- jsapp/src/jsx/xui.jsx
+++ jsapp/src/jsx/xui.jsx
@@ -103,6 +103,7 @@
 			if (this.props.item.id == "M_OVERVIEW") {
 				ReactDOM.render(<OverViewPage auto_update={true}/>, document.getElementById("main"));
 			} else if (this.props.item.id == "M_CALLS") {
+				ReactDOM.render(<CallsPage/>, document.getElementById("main"));
 			} else if (this.props.item.id == "M_CHANNELS") {
 				ReactDOM.render(<ChannelsPage/>, document.getElementById("main"));
 			} else if (this.props.item.id == "M_USERS") {
@@ -188,6 +189,174 @@
 
 		render: function() {
 			return <div><pre>{this.state.msg}</pre></div>;
+		}
+	});
+
+	var CallsPage = React.createClass({
+		getInitialState: function() {
+			return {rows: []};
+		},
+
+		handleClick: function(x) {
+		},
+
+		componentWillMount: function() {
+		},
+
+		componentWillUnmount: function() {
+			verto.unsubscribe("FSevent.channel_create");
+			verto.unsubscribe("FSevent.channel_progress");
+			verto.unsubscribe("FSevent.channel_answer");
+			verto.unsubscribe("FSevent.channel_bridge");
+			verto.unsubscribe("FSevent.channel_hangup");
+			// verto.unsubscribe("FSevent");
+		},
+
+		componentDidMount: function() {
+			var _this = this;
+			showFSAPI("calls", function(data) {
+				var msg = $.parseJSON(data.message);
+				if (msg.row_count === 0) {
+					_this.setState({rows: []});
+				} else {
+					console.log(msg.rows);
+					_this.setState({rows: msg.rows});
+				};
+			});
+
+			verto.subscribe("FSevent.channel_create", {
+				handler: this.handleFSEvent
+			});
+
+			verto.subscribe("FSevent.channel_progress", {
+				handler: this.handleFSEvent
+			});
+
+			verto.subscribe("FSevent.channel_answer", {
+				handler: this.handleFSEvent
+			});
+
+			verto.subscribe("FSevent.channel_bridge", {
+				handler: this.handleFSEvent
+			});
+
+			verto.subscribe("FSevent.channel_hangup", {
+				handler: this.handleFSEvent
+			});
+
+			// verto.subscribe("FSevent", {
+			// 	handler: _this.handleFSEvent
+			// });
+		},
+
+		handleFSEvent: function(v, e) {
+			console.log("FSevent:", e);
+			if (e.eventChannel == "FSevent.channel_create") {
+				var rows = this.state.rows;
+				var row = {};
+				var date = new Date(parseInt(e.data["Caller-Channel-Created-Time"]) / 1000).toISOString();
+
+				row.uuid = e.data["Unique-ID"];
+				row.cid_num = e.data["Caller-Caller-ID-Number"];
+				row.dest = e.data["Caller-Destination-Number"];
+				row.callstate = e.data["Channel-Call-State"];
+				row.direction = e.data["Call-Direction"];
+				row.created = date;
+				rows.push(row);
+				this.setState({rows: rows});
+			} else if (e.eventChannel == "FSevent.channel_progress") {
+				var rows = [];
+				var uuid = e.data["Unique-ID"];
+
+				this.state.rows.forEach(function(row) {
+					if (uuid == row.uuid) {
+						row.callstate = e.data["Channel-Call-State"];
+					}
+					rows.push(row);
+				});
+
+				this.setState({rows: rows});
+
+			} else if (e.eventChannel == "FSevent.channel_answer") {
+				var rows = [];
+				var uuid = e.data["Unique-ID"];
+
+				this.state.rows.forEach(function(row) {
+					if (uuid == row.uuid) {
+						row.callstate = "Active";
+					}
+					rows.push(row);
+				});
+
+				this.setState({rows: rows});
+			} else if (e.eventChannel == "FSevent.channel_hangup") {
+				var rows = [];
+				var uuid = e.data["Unique-ID"];
+
+				// delete from rows, maybe find a more efficient way?
+				this.state.rows.forEach(function(row) {
+					if (uuid != row.uuid) {
+						rows.push(row);
+					}
+				});
+
+				this.setState({rows: rows});
+			} else if (e.eventChannel == "FSevent.channel_bridge") {
+				var rows = [];
+				var uuid = e.data["Unique-ID"];
+				console.log("eeeeeeee", e);
+				this.state.rows.forEach(function(row) {
+					if (row.uuid == uuid) {
+						var date = new Date(parseInt(e.data["Other-Leg-Channel-Created-Time"]) / 1000).toISOString();
+
+						row.callstate = e.data["Channel-Call-State"];
+						row.b_uuid = e.data["Other-Leg-Unique-ID"];
+						row.b_callstate = e.data["Other-Leg-Callstate"];
+						row.b_direction = e.data["Other-Leg-Direction"];
+						row.b_created = date;
+						rows.push(row);
+					} else if (row.uuid == e.data["Other-Leg-Unique-ID"]) {
+						// remove this channel
+					} else {
+						rows.push(row);
+					}
+				});
+
+				this.setState({rows: rows});
+			}
+		},
+
+		render: function() {
+			var rows = [];
+			this.state.rows.forEach(function(row) {
+				rows.push(<tr key={row.uuid}>
+						<td>{row.uuid}<br/>{row.b_uuid}</td>
+						<td>{row.cid_num}</td>
+						<td>{row.dest}</td>
+						<td>{row.callstate}<br/>{row.b_callstate}</td>
+						<td>{row.direction}<br/>{row.b_direction}</td>
+						<td>{row.created}<br/>{row.b_created}</td>
+				</tr>);
+			})
+
+			return <div>
+				<h1>Calls</h1>
+				<div>
+					<table className="table">
+					<tbody>
+					<tr>
+						<th>UUID</th>
+						<th>CID</th>
+						<th>Dest</th>
+						<th>Call State</th>
+						<th>Direction</th>
+						<th>Created</th>
+					</tr>
+					{rows}
+					</tbody>
+					</table>
+				</div>
+			</div>
 		}
 	});
 
