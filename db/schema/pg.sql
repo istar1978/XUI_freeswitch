d7811efff0ab61eaab53e05a4289efc840a9ef75
Seven Du <dujinfang@gmail.com>
2016-11-28 09:48:10 +0800

    cleanup

--- db/schema/pg.sql
+++ db/schema/pg.sql
@@ -1,10 +1,1 @@
-CREATE TABLE users (
-	id serial,
-	data jsonb
-);
-
-CREATE TABLE extensions (
-	id serial,
-	user_id integer,
-	data jsonb
-);
+-- todo
--- db/schema/sqlite.sql
+++ db/schema/sqlite.sql
@@ -1,12 +1,39 @@
-CREATE TABLE contexts (
+CREATE TABLE routes (
 	id INTEGER PRIMARY KEY,
 	name VARCHAR,
-	data TEXT
+	description VARCHAR,
+	prefix VARCHAR,
+	length, INTEGER,
+	context VARCHAR,
+	dnc VARCHAR,
+	sdnc VARCHAR,
+	dest_type VARCHAR,
+	dest_uuid VARCHAR,
+	body TEXT
 );
 
-
-CREATE TABLE routings (
+CREATE TABLE users (
 	id INTEGER PRIMARY KEY,
-	context_id INTEGER,
-	data TEXT
+	extn VARCHAR,
+	name VARCHAR,
+	cid_number VARCHAR,
+	cid_name VARCHAR,
+	context VARCHAR,
+	domain VARCHAR,
+	password VARCHAR,
+	vm_password VARCHAR,
+	type VARCHAR,
+	disabled VARCHAR
+);
+
+INSERT INTO users (extn, name, password) VALUES ('admin', 'Admin', 'admin');
+
+CREATE TABLE blocks (
+	id INTEGER PRIMARY KEY,
+	name VARCHAR,
+	description VARCHAR,
+	created_at_epoch INTEGER,
+	xml TEXT,
+	js TEXT,
+	lua TEXT
 );
