33a2343d806a945e3c97d0614d736e5048f666dc
Seven Du <dujinfang@gmail.com>
2016-12-23 22:17:22 +0800

    fix post_load_modules

--- lua/xui/fsxml/configuration/post_load_modules.conf.lua
+++ lua/xui/fsxml/configuration/post_load_modules.conf.lua
@@ -1,21 +1,13 @@
 function build_modules_conf(modules)
 	local settings = ""
-	local cond = {realm = 'modules',disabled = 0}
+	local cond = {realm = 'modules', disabled = 0}
 
-	xdb.find_by_cond("params",cond,"id",function(row)
+	xdb.find_by_cond("params", cond, "id", function(row)
 		settings = settings .. '<load module="' .. row.k ..'"/>'
+	end)
+
 	return [[<modules>]] .. settings .. [[</modules>]]
 end
-local modules = nil
-local cond = {realm = 'modules',disabled = 0}
 
-xdb.find_by_cond("params",cond,"id",function(row))
-modules = row
-end)
-
-if (modules) then
-
-	XML_STRING = [[<configuration name="modules.conf" description="Modules">]] ..
+XML_STRING = [[<configuration name="post_load_modules.conf" description="Post Load Modules">]] ..
 		build_modules_conf(modules) .. [[</configuration>]]
-end
-
