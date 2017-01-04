a587c819e9bdf8753529f0ca4186a27a90a45f8d
Seven Du <dujinfang@gmail.com>
2017-01-04 13:36:03 +0800

    fix no backslashes in sqlite

--- db/schema/init_route.sql
+++ db/schema/init_route.sql
@@ -1,4 +1,4 @@
-INSERT INTO routes (name, description, prefix, context, dest_type, body) VALUES ('Echo', 'Echo Test', '9196', 'default', 'FS_DEST_SYSTEM', 'answer\necho');
+INSERT INTO routes (name, description, prefix, context, dest_type, body) VALUES ('Echo', 'Echo Test', '9196', 'default', 'FS_DEST_SYSTEM', 'answer' || x'0a' || echo');
 INSERT INTO routes (name, description, prefix, context, dest_type, body) VALUES ('Conference', 'Conference', '30', 'default', 'FS_DEST_SYSTEM', 'conference {$destination_number}-$${domain}');
 INSERT INTO routes (name, description, prefix, context, dest_type, body) VALUES ('User', 'Local Users', '1', 'default', 'FS_DEST_USER', NULL);
 INSERT INTO routes (name, description, prefix, context, dest_type, body) VALUES ('User', 'Local Users', '1', 'public', 'FS_DEST_USER', NULL);
