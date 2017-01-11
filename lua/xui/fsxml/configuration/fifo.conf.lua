71fdd1a545717577dfa43f120480ab6485042e6a
Seven Du <dujinfang@gmail.com>
2017-01-11 15:15:45 +0800

    update fifo conf

--- lua/xui/fsxml/configuration/fifo.conf.lua
+++ lua/xui/fsxml/configuration/fifo.conf.lua
@@ -10,9 +10,9 @@ XML_STRING=[[
       <!--<member timeout="60" simo="1" lag="20">{member_wait=nowait}user/1005@$${domain}</member>-->
     </fifo>
 
-    <fifo name="default" importance="0">
-      <member timeout="60" simo="1" lag="2" outbound_per_cycle="2">{member_wait=nowait}user/1001@$${domain}</member>
-      <member timeout="60" simo="1" lag="2" outbound_per_cycle="2">{member_wait=nowait}user/1002@$${domain}</member>
+    <fifo name="default" importance="0" outbound_per_cycle="2" outbound_per_cycle_min="2">
+      <member timeout="60" simo="1" lag="2">{member_wait=nowait}user/1001@$${domain}</member>
+      <member timeout="60" simo="1" lag="2">{member_wait=nowait}user/1002@$${domain}</member>
     </fifo>
 
   </fifos>
