abe84680a7b7c0a6f939f7756b517222ecdea794
Seven Du <dujinfang@gmail.com>
2017-02-15 11:10:14 +0800

    add temp audio record route

--- db/schema/init_route.sql
+++ db/schema/init_route.sql
@@ -2,6 +2,7 @@ INSERT INTO routes (name, description, prefix, context, dest_type, body) VALUES 
 INSERT INTO routes (name, description, prefix, context, dest_type, body) VALUES ('Conference', 'Conference', '30', 'default', 'FS_DEST_SYSTEM', 'conference ${destination_number}-$${domain}');
 INSERT INTO routes (name, description, prefix, context, dest_type, body) VALUES ('User', 'Local Users', '1', 'default', 'FS_DEST_USER', NULL);
 INSERT INTO routes (name, description, prefix, context, dest_type, body) VALUES ('User', 'Local Users', '1', 'public', 'FS_DEST_USER', NULL);
+INSERT INTO routes (name, description, prefix, context, dest_type, body) VALUES ('Record', 'Record', '*991234', 'default', 'FS_DEST_SYSTEM', 'answer' || x'0a' || 'lua audio_record.lua');
 
 INSERT INTO routes (name, description, prefix, context, dest_type, body) VALUES ('Fifo', 'Fifo', '*66', 'default', 'FS_DEST_SYSTEM',
 	'answer' || x'0a' || 'set a=${fifo_member(add default user/${caller_id_number})}' || x'0a' || 'playback tone_stream://%(100,1000,800);loops=1' || x'0a' || 'playback ivr/ivr-you_are_now_logged_in.wav');
