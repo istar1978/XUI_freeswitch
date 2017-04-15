00c15431e98163278a421809346c3c22b3b99110
Mariah Yang <galm@163.com>
2017-04-15 18:18:41 +0800

    support dynamic rtp_mcast

--- lua/xui/fsxml/configuration/rtp_mcast.conf.lua
+++ lua/xui/fsxml/configuration/rtp_mcast.conf.lua
@@ -1,7 +1,17 @@
-function build_mcasts()
-	local mcasts = ""
+local mcast_name = nil
+if params then
+	mcast_name = params:getHeader("mcast_name")
+end
 
-	xdb.find_by_cond("mcasts", nil, "id", function(row)
+function build_mcasts(mcast_name)
+	local mcasts = ""
+	local cond = nil
+
+	if mcast_name then
+		cond = {name = mcast_name}
+	end
+
+	xdb.find_by_cond("mcasts", cond, "id", function(row)
 		local p = '<param name="source"' .. ' value="' .. row.source .. '"/>'
 		p = p .. '<param name="codec-name"' .. ' value="' .. row.codec_name .. '"/>'
 		p = p .. '<param name="sample-rate"' .. ' value="' .. row.sample_rate .. '"/>'
@@ -20,5 +30,6 @@ function build_mcasts()
 end
 
 
-mcasts = build_mcasts()
-XML_STRING = [[<configuration name="rtp_mcast.conf" description="rtp mcast">]] .. mcasts .. [[</configuration>]]
+mcasts = build_mcasts(mcast_name)
+
+XML_STRING = [[<configuration name="rtp_mcast.conf" description="rtp mcast"><mcasts>]] .. mcasts .. [[</mcasts></configuration>]]
