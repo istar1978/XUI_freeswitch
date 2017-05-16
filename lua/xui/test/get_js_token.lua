920420672f0743574322eed4b760a2d4adfab666
Seven Du <dujinfang@gmail.com>
2017-05-17 03:49:13 +0800

    add test scripts

---
+++ lua/xui/test/get_js_token.lua
@@ -0,0 +1,24 @@
+-- print("test start ... \n")
+
+local cur_dir = debug.getinfo(1).source;
+cur_dir = string.gsub(cur_dir, "^@(.+)/test/get_js_token.lua$", "%1")
+package.path = package.path .. ";" .. cur_dir .. "/vendor/?.lua"
+package.path = package.path .. ";" .. cur_dir .. "/model/?.lua"
+
+-- stream:write(package.path)
+-- stream:write("test start\n")
+
+require 'xdb'
+require 'xwechat'
+require 'm_dict'
+require 'xtra_config'
+require 'utils'
+
+if config.db_auto_connect then xdb.connect(config.dsn) end
+
+realm = argv[1] or 'xyt'
+
+local wechat = m_dict.get_obj('WECHAT/' .. realm)
+token = xwechat.get_token(realm, wechat.APPID, wechat.APPSEC)
+
+stream:write("js_token: " .. token .. "\n")
---
+++ lua/xui/test/test_send_msg.lua
@@ -0,0 +1,38 @@
+print("test start ... \n")
+
+local cur_dir = debug.getinfo(1).source;
+cur_dir = string.gsub(cur_dir, "^@(.+)/test/test_send_msg.lua$", "%1")
+package.path = package.path .. ";" .. cur_dir .. "/vendor/?.lua"
+package.path = package.path .. ";" .. cur_dir .. "/model/?.lua"
+
+-- stream:write(package.path)
+-- stream:write("test start\n")
+
+require 'xdb'
+require 'xwechat'
+require 'm_dict'
+require 'xtra_config'
+require 'utils'
+
+if config.db_auto_connect then xdb.connect(config.dsn) end
+
+realm   = argv[1]
+extn    = argv[2]
+subject = argv[3]
+content = argv[4]
+user = xdb.find_one("users", {extn = extn})
+
+local weuser = xdb.find_one("wechat_users", {
+	user_id = user.id
+})
+
+if weuser then
+	local wechat = m_dict.get_obj('WECHAT/' .. realm)
+	token = xwechat.access_token('realm')
+	freeswitch.consoleLog("ERR", xwechat.access_token(realm))
+
+	redirect_uri = config.wechat_base_url .. "/api/wechat/" .. realm .. "/tickets/0"
+	redirect_uri = xwechat.redirect_uri(wechat.APPID, redirect_uri, "200")
+
+	xwechat.send_ticket_notification(realm, weuser.openid, redirect_uri, subject, content)
+end
